7ce679af4b2c8ddae9e180a7cfe28a29cb56d034
Loh Jing Yen <lohjingyen@gmail.com>
2017-12-12 21:15:38 +0800

    Do a crapton of styling

--- src/components/FuckYouUpload.vue
+++ src/components/FuckYouUpload.vue
@@ -1,43 +1,48 @@
 <template>
   <section class="section">
-    <div class="container is-centered">
+    <div class="columns is-centered">
+      <div class="column is-half">
 
-      <h2 class="subtitle">Response</h2>
-      <div class="content">
-        <pre>{{ data }}</pre>
+        <h1 class="title">Sibei amazing upload responder</h1>
+
+        <b-field>
+          <b-upload v-model="dropFiles"
+                    accept="image/*"
+                    drag-drop>
+            <section class="hero is-medium">
+              <div class="hero-body has-text-centered">
+                <p>
+                  <b-icon icon="upload"
+                          size="is-large">
+                  </b-icon>
+                </p>
+                <p>Click or drop also can, but only image ah, also MUST HAVE FACE one hor !!!</p>
+              </div>
+            </section>
+          </b-upload>
+        </b-field>
+
+        <div class="tags">
+          <span v-for="(file, index) in dropFiles"
+                :key="index"
+                class="tag is-primary">
+            {{file.name}}
+            <button class="delete is-small"
+                    type="button"
+                    @click="deleteDropFile(index)">
+            </button>
+          </span>
+        </div>
+
+        <button class="button is-danger"
+                @click="sendToAzure">Ok steady</button>
+
+        <h2 class="subtitle">Response</h2>
+        <div class="content">
+          <pre>{{ data }}</pre>
+        </div>
+
       </div>
-
-      <b-field>
-        <b-upload v-model="dropFiles"
-                  accept="image/*"
-                  drag-drop>
-          <section class="section">
-            <div class="content has-text-centered">
-              <p>
-                <b-icon icon="upload"
-                        size="is-large">
-                </b-icon>
-              </p>
-              <p>Click or drop, both also can</p>
-            </div>
-          </section>
-        </b-upload>
-      </b-field>
-
-      <div class="tags">
-        <span v-for="(file, index) in dropFiles"
-              :key="index"
-              class="tag is-primary">
-          {{file.name}}
-          <button class="delete is-small"
-                  type="button"
-                  @click="deleteDropFile(index)">
-          </button>
-        </span>
-      </div>
-
-      <button class="button is-primary"
-              @click="sendToAzure">Ok steady</button>
     </div>
   </section>
 </template>
@@ -48,7 +53,7 @@ import axios from 'axios'
 export default {
   data () {
     return {
-      data: null,
+      data: `Now got nothing la`,
       dropFiles: []
     }
   },
@@ -67,7 +72,7 @@ export default {
       }
       const url = 'https://southeastasia.api.cognitive.microsoft.com/face/v1.0/facelists/iotfaces1/persistedFaces?userData='
       const name = 'kaixiang'
-      const apiKey = 'YOUR-API-KEY' // PUT THE API KEY HERE
+      const apiKey = 'YOUR API KEY' // PUT THE API KEY HERE
 
       axios.post(`${url}${name}`, this.dropFiles[0], {
         headers: {
@@ -75,12 +80,12 @@ export default {
           'Content-Type': 'application/octet-stream'
         }
       })
-      .then(res => {
-        console.log(res.data)
-        this.data = res.data
-      })
-      .catch(e => console.error(e))
+      .then(res => this.data = res.data)
+      .catch(e => this.data = e)
     }
   }
 }
 </script>
+
+<style scoped>
+</style>
